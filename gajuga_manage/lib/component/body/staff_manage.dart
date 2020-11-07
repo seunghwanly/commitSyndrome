491e7755c2619274084e024d2730ca62eae20ba5
Juyeong Lee <2jy22@naver.com>
2020-11-08 04:21:39 +0900

    staff, sales, stock 화면 기본 틀

---
+++ gajuga_manage/lib/component/body/staff_manage.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:gajuga_manage/util/box_shadow.dart';
+import 'package:gajuga_manage/util/palette.dart';
+import 'package:gajuga_manage/util/to_text.dart';
+
+class StaffManage extends StatefulWidget {
+  @override
+  _StaffManageState createState() => _StaffManageState();
+}
+
+class _StaffManageState extends State<StaffManage> {
+  TextEditingController searchController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.all(40),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(40),
+            topRight: Radius.circular(40),
+          ),
+          color: Colors.white,
+        ),
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                makeTitle('직원', ' 프로필 수정'),
+                IconButton(
+                  icon: Icon(Icons.keyboard_arrow_down),
+                  onPressed: () { },
+                ),
+              ],
+            ),
+            Divider(
+              height: 20,
+              thickness: 3,
+              indent: 14,
+              color: darkgrey,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
