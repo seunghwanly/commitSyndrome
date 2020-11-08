488a4782939150eafaf0ace31006b7aad3a039f3
Juyeong Lee <2jy22@naver.com>
2020-11-08 17:47:54 +0900

    [WIP] 직원 프로필 수정

--- gajuga_manage/lib/component/body/staff/staff_profile.dart
+++ gajuga_manage/lib/component/body/staff/staff_profile.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:gajuga_manage/util/box_shadow.dart';
 import 'package:gajuga_manage/util/palette.dart';
 import 'package:gajuga_manage/util/to_text.dart';
+import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
 
 class StaffProfile extends StatefulWidget {
   @override
@@ -9,8 +12,11 @@ class StaffProfile extends StatefulWidget {
 }
 
 class _StaffProfileState extends State<StaffProfile> {
-  TextEditingController searchController = new TextEditingController();
   final List<String> data = <String>['김관우', '박종하', '이승환', '이주영'];
+  TextEditingController searchController = new TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  File _profileImage;
+  final picker = ImagePicker();
 
   @override
   Widget build(BuildContext context) {
@@ -39,24 +45,28 @@ class _StaffProfileState extends State<StaffProfile> {
                 ),
               ],
             ),
-            Container(
-              padding: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height * 0.03,
-              ),
-              height: MediaQuery.of(context).size.height * 0.4,
-              width: double.infinity,
-              child: ListView.builder(
-                itemCount: data.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return _listItem(
-                    data[index],
-                    AssetImage('images/default_profile.jpg'),
-                    '직원',
-                    '010-1234-1234',
-                    context,
-                  );
-                },
-                scrollDirection: Axis.horizontal,
+            Expanded(
+              child: SingleChildScrollView(
+                child: Container(
+                  padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.03,
+                  ),
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  width: double.infinity,
+                  child: ListView.builder(
+                    itemCount: data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return _listItem(
+                        data[index],
+                        AssetImage('images/default_profile.jpg'),
+                        '직원',
+                        '010-1234-1234',
+                        context,
+                      );
+                    },
+                    scrollDirection: Axis.horizontal,
+                  ),
+                ),
               ),
             ),
           ],
@@ -98,7 +108,9 @@ class _StaffProfileState extends State<StaffProfile> {
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(20),
             ),
-            onPressed: () { },
+            onPressed: () {
+              showProfileDialog(context);
+            },
             child: Container(
               alignment: Alignment.center,
               child: Text(
@@ -114,6 +126,265 @@ class _StaffProfileState extends State<StaffProfile> {
           ),
         ],
       ),
+    );
+  }
+
+  Future showProfileDialog(BuildContext context) {
+    return showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        content: Stack(
+          overflow: Overflow.visible,
+          children: <Widget>[
+            Positioned(
+              right: -40.0,
+              top: -40.0,
+              child: InkResponse(
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+                child: CircleAvatar(
+                  child: Icon(
+                    Icons.close,
+                    color: Colors.white,
+                  ),
+                  backgroundColor: Colors.red,
+                ),
+              ),
+            ),
+            Form(
+              key: _formKey,
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    RichText(
+                      text: TextSpan(
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: '직원',
+                            style: TextStyle(color: darkgrey),
+                          ),
+                          TextSpan(
+                            text: ' 프로필 수정',
+                            style: TextStyle(color: orange),
+                          ),
+                        ],
+                      ),
+                    ),
+                    userImageView(),
+                    SizedBox(height: 20),
+                    nameField(),
+                    phoneField(),
+                    addressField(),
+                    accountNumberField(),
+                    SizedBox(height: 10),
+                    FlatButton(
+                      color: orange,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Container(
+                        alignment: Alignment.center,
+                        child: Text(
+                          "저장하고 닫기",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+
+  Widget userImageView() {
+    return Container(
+      margin: EdgeInsets.only(top: 30),
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Container(
+            child: CircleAvatar(
+              radius: 65.0,
+              backgroundImage: _profileImage != null
+                  ? Image.file(_profileImage)
+                  : AssetImage('images/default_profile.jpg'),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Color(0xFFe0e0e0)),
+                borderRadius: BorderRadius.circular(50.0)),
+              padding: EdgeInsets.all(0),
+              alignment: Alignment.center,
+              child: IconButton(
+                onPressed: () { },
+                icon: Icon(
+                  Icons.camera_alt,
+                  size: 20,
+                  color: Color(0xFFbbbbbb),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future getProfileImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null) {
+        _profileImage = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  Widget nameField() {
+    return Row(
+      children: [
+        Text(
+          '이       름',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Flexible(
+          child: Container(
+            padding: EdgeInsets.all(8),
+            child: TextFormField(
+              keyboardType: TextInputType.text,
+              decoration: new InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(40),
+                  borderSide: BorderSide(color: Colors.grey[300]),
+                ),
+                hintText: '이름',
+                isDense: true,
+                contentPadding: EdgeInsets.fromLTRB(15, 15, 15, 0),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget phoneField() {
+    return Row(
+      children: [
+        Text(
+          '전화번호',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Flexible(
+          child: Container(
+            padding: EdgeInsets.all(8),
+            child: TextFormField(
+              keyboardType: TextInputType.text,
+              decoration: new InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(40),
+                  borderSide: BorderSide(color: Colors.grey[300]),
+                ),
+                hintText: '전화번호',
+                isDense: true,
+                contentPadding: EdgeInsets.fromLTRB(15, 15, 15, 0),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget addressField() {
+    return Row(
+      children: [
+        Text(
+          '주       소',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Flexible(
+          child: Container(
+            padding: EdgeInsets.all(8),
+            child: TextFormField(
+              keyboardType: TextInputType.text,
+              decoration: new InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(40),
+                  borderSide: BorderSide(color: Colors.grey[300]),
+                ),
+                hintText: '주소',
+                isDense: true,
+                contentPadding: EdgeInsets.fromLTRB(15, 15, 15, 0),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget accountNumberField() {
+    return Row(
+      children: [
+        Text(
+          '계좌번호',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Flexible(
+          child: Container(
+            padding: EdgeInsets.all(8),
+            child: TextFormField(
+              keyboardType: TextInputType.text,
+              decoration: new InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(40),
+                  borderSide: BorderSide(color: Colors.grey[300]),
+                ),
+                hintText: '계좌번호',
+                isDense: true,
+                contentPadding: EdgeInsets.fromLTRB(15, 15, 15, 0),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
