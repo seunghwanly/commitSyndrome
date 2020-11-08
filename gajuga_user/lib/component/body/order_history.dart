95089065a5c1ee4c284ce520fd3b7c131142dd18
Seunghwanly <seunghwanly@gmail.com>
2020-11-09 01:59:33 +0900

    loading and firebase read test

--- gajuga_user/lib/component/body/order_history.dart
+++ gajuga_user/lib/component/body/order_history.dart
@@ -6,6 +6,10 @@ import '../../util/box_shadow.dart';
 import '../../util/to_text.dart';
 import '../../util/palette.dart';
 import '../../util/to_locale.dart';
+//firebase database
+import 'package:firebase_database/firebase_database.dart';
+//loading
+import 'package:loading_animations/loading_animations.dart';
 
 class OrderHistory extends StatefulWidget {
   //OrderHistory ({  });
@@ -15,6 +19,10 @@ class OrderHistory extends StatefulWidget {
 }
 
 class OrderHistoryState extends State<OrderHistory> {
+  final databaseReference = FirebaseDatabase.instance.reference();
+
+  var fetchedData;
+
   final data = [
     {
       'customer': '01012341234',
@@ -72,129 +80,158 @@ class OrderHistoryState extends State<OrderHistory> {
   ];
 
   @override
+  void initState() {
+    super.initState();
+    readData();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return CustomHeader(
-        body: SingleChildScrollView(
-            child: Column(
-      children: [
-        makeTitle("주문", "내역"),
-        ListView.builder(
-          physics: ClampingScrollPhysics(),
-          shrinkWrap: true,
-          itemCount: data.length,
-          itemBuilder: (BuildContext context, int index) {
-            var orders = new Map<String, dynamic>.from(data[index]);
+    if (this.fetchedData == null) {
+      return Container(
+        color: pale,
+        child: LoadingBouncingGrid.circle(
+        backgroundColor: white,
+        ),
+      );
+    } else {
+      return CustomHeader(
+          body: SingleChildScrollView(
+              child: Column(
+        children: [
+          makeTitle("주문", "내역"),
+          ListView.builder(
+            physics: ClampingScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: data.length,
+            itemBuilder: (BuildContext context, int index) {
+              // var orders = new Map<String, dynamic>.from(data[index]);
+              var orders =
+                  new Map<String, dynamic>.from(this.fetchedData[index]);
 
-            int totalCost = 0;
+              int totalCost = 0;
 
-            for (int i = 0; i < orders['content'].length; ++i) {
-              totalCost += orders['content'][i]['cost'];
-            }
+              for (int i = 0; i < orders['content'].length; ++i) {
+                totalCost += orders['content'][i]['cost'];
+              }
 
-            return customBoxContainerWithMargin(
-                MediaQuery.of(context).size.width * 0.9, // 가로 세로 1 : 2 비율
-                MediaQuery.of(context).size.width * 0.5,
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(top: 10.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          makeTitleSize('피자', ' PIZZA', 0.0, 16, true),
-                          Text(
-                            DateTime.now()
-                                    .difference(
-                                        DateTime.parse(orders['order_time']))
-                                    .inDays
-                                    .toString() +
-                                ' 일전',
-                            style: TextStyle(
-                                color: darkblue,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16),
-                          )
-                        ],
+              return customBoxContainerWithMargin(
+                  MediaQuery.of(context).size.width * 0.9, // 가로 세로 1 : 2 비율
+                  MediaQuery.of(context).size.width * 0.5,
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            makeTitleSize('피자', ' PIZZA', 0.0, 16, true),
+                            Text(
+                              DateTime.now()
+                                      .difference(
+                                          DateTime.parse(orders['order_time']))
+                                      .inDays
+                                      .toString() +
+                                  ' 일전',
+                              style: TextStyle(
+                                  color: darkblue,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16),
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(top: 10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          CircleAvatar(
-                            radius: 30,
-                            backgroundImage: AssetImage('images/A.png'),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              makeTextSize(
-                                  '주문번호 ' + orders['order_number'].toString(),
-                                  darkgrey,
-                                  20.0,
-                                  14),
-                              makeTextSize(
-                                  orders['content'][0]['name'] +
-                                      (orders['content'].length - 1 > 0
-                                          ? ' 외 ' +
-                                              (orders['content'].length - 1)
-                                                  .toString() +
-                                              '개'
-                                          : ''),
-                                  darkblue,
-                                  20.0,
-                                  14),
-                              makeTextSize(toLocaleString(totalCost) + ' 원',
-                                  lightgrey, 20.0, 14)
-                            ],
-                          )
-                        ],
+                      Container(
+                        margin: EdgeInsets.only(top: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            CircleAvatar(
+                              radius: 30,
+                              backgroundImage: AssetImage('images/A.png'),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                makeTextSize(
+                                    '주문번호 ' + orders['order_number'].toString(),
+                                    darkgrey,
+                                    20.0,
+                                    14),
+                                makeTextSize(
+                                    orders['content'][0]['name'] +
+                                        (orders['content'].length - 1 > 0
+                                            ? ' 외 ' +
+                                                (orders['content'].length - 1)
+                                                    .toString() +
+                                                '개'
+                                            : ''),
+                                    darkblue,
+                                    20.0,
+                                    14),
+                                makeTextSize(toLocaleString(totalCost) + ' 원',
+                                    lightgrey, 20.0, 14)
+                              ],
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                    Container(
-                      alignment: Alignment.centerRight,
-                      child: FlatButton(
-                        color: orange,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                        onPressed: () {
-                          showOrderHistoryModal(context, orders);
-                        },
-                        child: Container(
-                          width: MediaQuery.of(context).size.width * 0.32,
-                          height: MediaQuery.of(context).size.width * 0.08,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              new Icon(
-                                Icons.receipt,
-                                color: white,
-                                size: 15,
-                              ),
-                              new Text(
-                                "전자영수증보기",
-                                style: TextStyle(
-                                    color: white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 12),
-                              )
-                            ],
+                      Container(
+                        alignment: Alignment.centerRight,
+                        child: FlatButton(
+                          color: orange,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                          onPressed: () {
+                            showOrderHistoryModal(context, orders);
+                          },
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.32,
+                            height: MediaQuery.of(context).size.width * 0.08,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                new Icon(
+                                  Icons.receipt,
+                                  color: white,
+                                  size: 15,
+                                ),
+                                new Text(
+                                  "전자영수증보기",
+                                  style: TextStyle(
+                                      color: white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 12),
+                                )
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                true,
-                MediaQuery.of(context).size.height * 0.015, // 12.18 / 812
-                MediaQuery.of(context).size.width * 0.05); // 20 / 375
-          },
-        ),
-      ],
-    )));
+                    ],
+                  ),
+                  true,
+                  MediaQuery.of(context).size.height * 0.015, // 12.18 / 812
+                  MediaQuery.of(context).size.width * 0.05); // 20 / 375
+            },
+          ),
+        ],
+      )));
+    }
+  }
+
+  void readData() {
+    databaseReference
+        .child('user/history')
+        .once()
+        .then((DataSnapshot dataSnapshot) {
+      setState(() {
+        this.fetchedData = dataSnapshot.value;
+      });
+      // fetchedData = dataSnapshot.value;
+    });
   }
 }
