a89ecad8cefbcbe9236ae23abfa897ff14a933ad
Seunghwanly <seunghwanly@gmail.com>
2020-11-10 22:15:04 +0900

    관리자 재고관리 UI/UX 완료

--- gajuga_manage/lib/component/body/stock/stock_manage.dart
+++ gajuga_manage/lib/component/body/stock/stock_manage.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+//pages
+import 'package:gajuga_manage/component/body/stock/stock_current.dart';
+import 'package:gajuga_manage/component/body/stock/stock_add.dart';
+import 'package:gajuga_manage/component/body/stock/stock_order.dart';
+//util
 import 'package:gajuga_manage/util/box_button.dart';
 import 'package:gajuga_manage/util/palette.dart';
 import 'package:gajuga_manage/util/to_text.dart';
 //date picker
 import '../../../util/date_picker.dart';
+import 'stock_current.dart';
 
 class StockManage extends StatefulWidget {
   @override
@@ -14,11 +20,30 @@ class StockManage extends StatefulWidget {
 class _StockManageState extends State<StockManage> {
   TextEditingController searchController = new TextEditingController();
 
+  var pageIndex = 0;
+  bool save = false;
+
   void handleRequest() {
-    print('');
+    setState(() {
+      this.pageIndex = 2;
+    });
   }
 
-  void handleAdd() {}
+  void handleAdd() {
+    setState(() {
+      this.pageIndex = 1;
+    });
+  }
+
+  void handleCancel() {
+    setState(() {
+      this.pageIndex = 0;
+    });
+  }
+
+  void handleSave() {
+    showModal();
+  }
 
   DateTime selectedDate;
 
@@ -35,80 +60,102 @@ class _StockManageState extends State<StockManage> {
             color: Colors.white,
           ),
           width: MediaQuery.of(context).size.width,
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.start,
-            // crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                flex: 1,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      flex: 3,
-                      child: makeTitle('재고', ' 확인'),
-                    ),
-                    Expanded(
-                      flex: 4,
-                      child: Container(
-                          alignment: Alignment.center,
-                          child: datePicker(context, selectedDate)),
-                    ),
-                    Expanded(
-                        flex: 3,
+          child: SingleChildScrollView(
+              //controller: new ScrollController(keepScrollOffset: true),
+              child: SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    // mainAxisAlignment: MainAxisAlignment.start,
+                    // crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        flex: 1,
                         child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            tapButton(
-                                handleAdd,
-                                darkblue,
-                                orange,
-                                "재고 입력",
-                                white,
-                                18.0,
-                                MediaQuery.of(context).size.width *
-                                    0.10, //width
-                                MediaQuery.of(context).size.height *
-                                    0.05, //height
-                                MediaQuery.of(context).size.height *
-                                    0.01, //padding V
-                                MediaQuery.of(context).size.width *
-                                    0.01, //padding H
-                                0.0),
-                            tapButton(
-                                handleRequest,
-                                darkblue,
-                                orange,
-                                "발주 신청",
-                                white,
-                                18.0,
-                                MediaQuery.of(context).size.width *
-                                    0.10, //width
-                                MediaQuery.of(context).size.height *
-                                    0.05, //height
-                                MediaQuery.of(context).size.height *
-                                    0.01, //padding V
-                                MediaQuery.of(context).size.width *
-                                    0.01, //padding H
-                                0.0), // margin H
+                            Expanded(
+                                flex: 3,
+                                child: GestureDetector(
+                                  onTap: () => setState(() {
+                                    this.pageIndex = 0;
+                                  }),
+                                  child: this.pageIndex == 0
+                                      ? makeTitle('재고', ' 확인')
+                                      : this.pageIndex == 1
+                                          ? makeTitle("재고", " 입력")
+                                          : makeTitle("발주", " 신청"),
+                                )),
+                            Expanded(
+                              flex: 4,
+                              child: Container(
+                                  alignment: Alignment.center,
+                                  child: datePicker(context, selectedDate)),
+                            ),
+                            Expanded(
+                                flex: 3,
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    tapButton(
+                                        this.pageIndex == 0
+                                            ? handleAdd
+                                            : handleCancel,
+                                        this.pageIndex == 0
+                                            ? darkblue
+                                            : superlight,
+                                        orange,
+                                        this.pageIndex == 0 ? "재고 입력" : "취소",
+                                        this.pageIndex == 0 ? white : darkblue,
+                                        18.0,
+                                        MediaQuery.of(context).size.width *
+                                            0.10, //width
+                                        MediaQuery.of(context).size.height *
+                                            0.05, //height
+                                        MediaQuery.of(context).size.height *
+                                            0.01, //padding V
+                                        MediaQuery.of(context).size.width *
+                                            0.01, //padding H
+                                        0.0),
+                                    tapButton(
+                                        this.pageIndex == 0
+                                            ? handleRequest
+                                            : handleSave,
+                                        this.pageIndex == 0 ? darkblue : orange,
+                                        orange,
+                                        this.pageIndex == 0 ? "발주 신청" : "저장",
+                                        white,
+                                        18.0,
+                                        MediaQuery.of(context).size.width *
+                                            0.10, //width
+                                        MediaQuery.of(context).size.height *
+                                            0.05, //height
+                                        MediaQuery.of(context).size.height *
+                                            0.01, //padding V
+                                        MediaQuery.of(context).size.width *
+                                            0.01, //padding H
+                                        0.0), // margin H
+                                  ],
+                                ))
                           ],
-                        ))
-                  ],
-                ),
-              ),
-              Expanded(
-                flex: 9,
-                child: mainBody(),
-              )
-            ],
-          )),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 9,
+                        child: mainBody(context, pageIndex, save),
+                      )
+                    ],
+                  )))),
     );
   }
 
-  RaisedButton datePicker(BuildContext c, DateTime selectedDate) {
+  datePicker(BuildContext c, DateTime selectedDate) {
     return RaisedButton(
-        onPressed: () {return customDatePicker(c);},
+        onPressed: () async {
+          return customDatePicker(c);
+        },
         color: white,
         splashColor: superlight,
         shape: RoundedRectangleBorder(
@@ -139,8 +186,85 @@ class _StockManageState extends State<StockManage> {
           ),
         ));
   }
+
+  showModal() {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Dialog(
+            elevation: 16.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            backgroundColor: white,
+            child: Container(
+              width: MediaQuery.of(context).size.width / 3,
+              height: MediaQuery.of(context).size.height / 5,
+              padding: EdgeInsets.fromLTRB(20, 40, 20, 20),
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    this.pageIndex == 1 ? "재고 입력이 완료되었습니다." : "발주 신청이 완료되었습니다.",
+                    style: TextStyle(
+                        color: darkblue,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0),
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        this.save = false;
+                        this.pageIndex = 0;
+                      });
+                      Navigator.pop(context);
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: MediaQuery.of(context).size.width / 12,
+                      height: MediaQuery.of(context).size.height / 20,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: orange),
+                      child: Text(
+                        '확인',
+                        style: TextStyle(
+                            color: white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.0),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        });
+  }
 }
 
-Widget mainBody() {
-  return Container();
+Widget mainBody(BuildContext context, int index, final save) {
+  switch (index) {
+    case 0:
+      return Container(
+        margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 0.0),
+        child: CurrentStock(),
+      );
+    case 1:
+      return Container(
+        margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 0.0),
+        child: AddStock(savePressed: save),
+      );
+    case 2:
+      return Container(
+        margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 0.0),
+        child: OrderStock(savePressed: save),
+      );
+    default:
+      return Container(
+        margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 0.0),
+        child: CurrentStock(),
+      );
+  }
 }
---
+++ gajuga_manage/lib/component/body/stock/stock_order.dart
@@ -0,0 +1,193 @@
+import 'package:flutter/material.dart';
+import 'package:gajuga_manage/util/palette.dart';
+
+import 'dart:math';
+
+class OrderStock extends StatefulWidget {
+  final savePressed;
+
+  OrderStock({this.savePressed});
+
+  @override
+  OrderStockState createState() => OrderStockState();
+}
+
+class OrderStockState extends State<OrderStock> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: Container(
+            // height: MediaQuery.of(context).size.height / 2,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                border: Border.all(color: Color.fromRGBO(238, 238, 238, 1.0))),
+            child: Column(
+              children: [
+                Expanded(
+                    flex: 1,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          flex: 4,
+                          child: Container(
+                              height: MediaQuery.of(context).size.height / 10,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(20.0)),
+                                color: orange,
+                              ),
+                              padding:
+                                  EdgeInsets.fromLTRB(30.0, 10.0, 0.0, 10.0),
+                              child: Row(
+                                children: [
+                                  Text(
+                                    "항 목",
+                                    style: TextStyle(
+                                        color: white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18),
+                                  ),
+                                  Icon(Icons.keyboard_arrow_down, color: white)
+                                ],
+                              )),
+                        ),
+                        Expanded(
+                          flex: 3,
+                          child: Container(
+                              height: MediaQuery.of(context).size.height / 10,
+                              padding:
+                                  EdgeInsets.fromLTRB(30.0, 10.0, 0.0, 10.0),
+                              decoration: BoxDecoration(
+                                color: orange,
+                              ),
+                              child: Row(
+                                children: [
+                                  Text(
+                                    "재 고",
+                                    style: TextStyle(
+                                        color: white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18),
+                                  ),
+                                  Icon(Icons.keyboard_arrow_down, color: white)
+                                ],
+                              )),
+                        ),
+                        Expanded(
+                          flex: 3,
+                          child: Container(
+                              height: MediaQuery.of(context).size.height / 10,
+                              padding:
+                                  EdgeInsets.fromLTRB(30.0, 10.0, 0.0, 10.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                    topRight: Radius.circular(20.0)),
+                                color: orange,
+                              ),
+                              child: Row(
+                                children: [
+                                  Text(
+                                    "주 문 량",
+                                    style: TextStyle(
+                                        color: white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18),
+                                  ),
+                                  Icon(Icons.keyboard_arrow_down, color: white)
+                                ],
+                              )),
+                        ),
+                      ],
+                    )),
+                Expanded(
+                    flex: 9,
+                    child: ListView.builder(
+                      //physics: NeverScrollableScrollPhysics(),
+                      itemCount: 10,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Column(
+                          children: [
+                            Container(
+                                margin: index == 0
+                                    ? EdgeInsets.only(top: 10.0)
+                                    : EdgeInsets.only(top: 0.0),
+                                height: MediaQuery.of(context).size.height / 12,
+                                width: MediaQuery.of(context).size.width,
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Expanded(
+                                      flex: 4,
+                                      child: Container(
+                                          padding: EdgeInsets.fromLTRB(
+                                              30.0, 10.0, 0.0, 10.0),
+                                          child: Text(
+                                            "항 목  " + index.toString(),
+                                            style: TextStyle(
+                                                color: darkblue,
+                                                fontWeight: FontWeight.w600,
+                                                fontSize: 16),
+                                          )),
+                                    ),
+                                    Expanded(
+                                      flex: 3,
+                                      child: Container(
+                                          padding: EdgeInsets.fromLTRB(
+                                              30.0, 10.0, 0.0, 10.0),
+                                          child: Text(
+                                            Random().nextInt(30).toString(),
+                                            style: TextStyle(
+                                                color: darkblue,
+                                                fontWeight: FontWeight.w600,
+                                                fontSize: 16),
+                                          )),
+                                    ),
+                                    Expanded(
+                                        flex: 3,
+                                        child: Row(
+                                          children: [
+                                            Expanded(
+                                                flex: 2,
+                                                child: Container(
+                                                  width: 100,
+                                                  padding: EdgeInsets.fromLTRB(
+                                                      30.0, 10.0, 0.0, 10.0),
+                                                  child: TextField(
+                                                    textAlign: TextAlign.center,
+                                                    decoration: InputDecoration(
+                                                        border: OutlineInputBorder(
+                                                            borderRadius:
+                                                                BorderRadius
+                                                                    .circular(
+                                                                        10)),
+                                                        labelText: "갯수",
+                                                        labelStyle: TextStyle(
+                                                            color: lightgrey,
+                                                            fontSize: 14.0,
+                                                            fontWeight:
+                                                                FontWeight
+                                                                    .w600)),
+                                                  ),
+                                                )),
+                                            Expanded(
+                                              flex: 3,
+                                              child: Container(),
+                                            )
+                                          ],
+                                        )),
+                                  ],
+                                )),
+                            Divider()
+                          ],
+                        );
+                      },
+                    )),
+              ],
+            )));
+  }
+}
