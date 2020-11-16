0e319f2e76bd770e47d0b3cdd0a3e7c24f22efd8
Seunghwanly <seunghwanly@gmail.com>
2020-11-17 00:51:15 +0900

    manager : 메뉴별 매출분석

--- gajuga_manage/lib/component/body/sales/sales_by_menu.dart
+++ gajuga_manage/lib/component/body/sales/sales_by_menu.dart
@@ -1,10 +1,13 @@
 import 'dart:math';
-import 'dart:ui';
 
-import 'package:bezier_chart/bezier_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:gajuga_manage/util/box_shadow.dart';
 import 'package:gajuga_manage/util/palette.dart';
+//pie chart
+import 'package:pie_chart/pie_chart.dart';
+
+//date picker
+import '../../../util/date_picker.dart';
 
 class SalesByMenu extends StatefulWidget {
   SalesByMenu();
@@ -14,104 +17,128 @@ class SalesByMenu extends StatefulWidget {
 }
 
 class _SalesByMenuState extends State<SalesByMenu> {
-  List<String> month = [
-    "1월",
-    "2월",
-    "3월",
-    "4월",
-    "5월",
-    "6월",
-    "7월",
-    "8월",
-    "9월",
-    "10월",
-    "11월",
-    "12월"
+  DateTime selectedDate;
+
+  //PIE CHART ELEMENTS
+  Map<String, double> todayData = {
+    "고르곤졸라피자": 3,
+    "포테이토피자": 2,
+    "페퍼로니피자": 5,
+    "불고기피자": 5
+  };
+
+  List<Color> todayDataColor = [
+    Color.fromRGBO(4, 157, 217, 1.0),
+    Color.fromRGBO(3, 101, 140, 1.0),
+    Color.fromRGBO(242, 194, 48, 1.0),
+    Color.fromRGBO(122, 191, 111, 1.0)
   ];
 
-  double handleMissingData(DateTime dateTime) {
-    return (Random().nextDouble() * 100).roundToDouble();
+  // LIFE CYCLE
+  @override
+  void initState() {
+    super.initState();
+    selectedDate = new DateTime.now();
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
-      height: MediaQuery.of(context).size.height / 2,
-      width: MediaQuery.of(context).size.width,
-      margin: EdgeInsets.only(
-        top: 20,
-      ),
-      decoration: customBoxDecoration(),
-      child: BezierChart(
-        bezierChartScale: BezierChartScale.MONTHLY,
-        fromDate: DateTime(2020, 01, 01),
-        toDate: DateTime.now(),
-        selectedDate: DateTime.now(),
-        series: [
-          BezierLine(
-            label: "고르곤졸라 피자",
-            lineColor: Color.fromRGBO(242, 230, 65, 1.0),
-            onMissingValue: handleMissingData,
-            data: [
-              DataPoint<DateTime>(value: 10, xAxis: DateTime(2020, 01, 01)),
-              DataPoint<DateTime>(value: 50, xAxis: DateTime(2020, 02, 01)),
-              DataPoint<DateTime>(value: 20, xAxis: DateTime(2020, 03, 01)),
-              DataPoint<DateTime>(value: 80, xAxis: DateTime(2020, 04, 01)),
-              DataPoint<DateTime>(value: 14, xAxis: DateTime(2020, 05, 01)),
-              DataPoint<DateTime>(value: 30, xAxis: DateTime(2020, 06, 01)),
+        alignment: Alignment.center,
+        height: MediaQuery.of(context).size.height / 2,
+        width: MediaQuery.of(context).size.width,
+        margin: EdgeInsets.only(
+          top: 20,
+        ),
+        decoration: customBoxDecoration(),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Container(
+                  margin: EdgeInsets.symmetric(vertical: 10.0),
+                  alignment: Alignment.center,
+                  child: datePicker(context)),
+            ),
+            Expanded(
+                flex: 8,
+                child: Row(
+                  //------------------------------------------------- PIE CHART & DESC
+                  children: [
+                    Expanded(
+                        //------------------------------------------------- PIE CHART
+                        flex: 6,
+                        child: PieChart(
+                          dataMap: todayData,
+                          colorList: todayDataColor,
+                        )),
+                    Expanded(
+                      //------------------------------------------------- DESC for 1st
+                      flex: 4,
+                      child: Column(
+                        children: [
+                          Expanded(
+                            flex: 4,
+                            child: CircleAvatar(
+                              backgroundImage: AssetImage('images/A.png'),
+                              radius: 70,
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: SizedBox(),
+                          ),
+                          Expanded(
+                            flex: 5,
+                            child: Text(
+                                "오늘은 ${todayData.keys.first} 가 제일 많은 판매량을 기록했네요 !\n 오늘 매출은 ${5000}원 입니다 ! 분발해주세요 !"),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ))
+          ],
+        ));
+  }
+
+  datePicker(BuildContext c) {
+    //------------------------------------------------- DATE PICKER FOR SETSTATE
+    return RaisedButton(
+        onPressed: () async {
+          DateTime newDateTime = await customDatePicker(c, selectedDate);
+          if (newDateTime != null) {
+            setState(() => selectedDate = newDateTime);
+          }
+        },
+        color: white,
+        splashColor: superlight,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+            side: BorderSide(color: superlight, width: 1.0)),
+        child: Container(
+          width: MediaQuery.of(c).size.width * 0.2,
+          height: MediaQuery.of(c).size.height * 0.05,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(
+                Icons.calendar_today,
+                color: lightgrey,
+              ),
+              Text(
+                selectedDate
+                    .toIso8601String()
+                    .substring(0, 10)
+                    .replaceAll('-', '/'),
+                style: TextStyle(color: lightgrey),
+              ),
+              Icon(
+                Icons.keyboard_arrow_down,
+                color: lightgrey,
+              )
             ],
           ),
-          BezierLine(
-            label: "포테이토 피자",
-            lineColor: Color.fromRGBO(3, 120, 166, 1.0),
-            onMissingValue: handleMissingData,
-            data: [
-              DataPoint<DateTime>(value: 100, xAxis: DateTime(2020, 01, 01)),
-              DataPoint<DateTime>(value: 70, xAxis: DateTime(2020, 02, 01)),
-              DataPoint<DateTime>(value: 50, xAxis: DateTime(2020, 03, 01)),
-              DataPoint<DateTime>(value: 10, xAxis: DateTime(2020, 04, 01)),
-              DataPoint<DateTime>(value: 14, xAxis: DateTime(2020, 05, 01)),
-              DataPoint<DateTime>(value: 60, xAxis: DateTime(2020, 06, 01)),
-            ],
-          ),
-          BezierLine(
-            label: "페퍼로니 피자",
-            lineColor: Color.fromRGBO(242, 135, 41, 1.0),
-            onMissingValue: handleMissingData,
-            data: [
-              DataPoint<DateTime>(value: 12, xAxis: DateTime(2020, 01, 01)),
-              DataPoint<DateTime>(value: 59, xAxis: DateTime(2020, 02, 01)),
-              DataPoint<DateTime>(value: 25, xAxis: DateTime(2020, 03, 01)),
-              DataPoint<DateTime>(value: 80, xAxis: DateTime(2020, 04, 01)),
-              DataPoint<DateTime>(value: 33, xAxis: DateTime(2020, 05, 01)),
-              DataPoint<DateTime>(value: 86, xAxis: DateTime(2020, 06, 01)),
-            ],
-          ),
-          BezierLine(
-            label: "불고기 피자",
-            lineColor: darkblue,
-            onMissingValue: handleMissingData,
-            data: [
-              DataPoint<DateTime>(value: 60, xAxis: DateTime(2020, 01, 01)),
-              DataPoint<DateTime>(value: 50, xAxis: DateTime(2020, 02, 01)),
-              DataPoint<DateTime>(value: 20, xAxis: DateTime(2020, 03, 01)),
-              DataPoint<DateTime>(value: 20, xAxis: DateTime(2020, 04, 01)),
-              DataPoint<DateTime>(value: 34, xAxis: DateTime(2020, 05, 01)),
-              DataPoint<DateTime>(value: 37, xAxis: DateTime(2020, 06, 01)),
-            ],
-          ),
-        ],
-        config: BezierChartConfig(
-            showVerticalIndicator: true,
-            verticalIndicatorStrokeWidth: 2.0,
-            verticalIndicatorColor: orange,
-            verticalIndicatorFixedPosition: false,
-            contentWidth: MediaQuery.of(context).size.width,
-            footerHeight: 50.0,
-            xAxisTextStyle: TextStyle(color: darkblue),
-            updatePositionOnTap: true),
-      ),
-    );
+        ));
   }
 }
