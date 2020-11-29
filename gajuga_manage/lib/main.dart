a989d8ad15e45288f8a2fd11c616ddf35b28cdeb
Seunghwanly <seunghwanly@gmail.com>
2020-11-29 16:29:06 +0900

    관리자 Firebase.configure 해결

--- gajuga_manage/lib/main.dart
+++ gajuga_manage/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gajuga_manage/model/sales_profit_model.dart';
+import 'package:gajuga_manage/util/loading.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -11,7 +12,6 @@ import 'model/menu_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
 
   runApp(MainScreen());
 }
@@ -20,32 +20,47 @@ class MainScreen extends StatelessWidget {
   static String userid = '';
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-        providers: [
-          ChangeNotifierProvider<FirebaseAuthService>(
-            create: (_) => FirebaseAuthService(),
-          ),
-          Provider<Information>.value(value: Information()),
-          ChangeNotifierProvider<Sales>(create: (_) => Sales())
-        ],
-        child: MaterialApp(
-          localizationsDelegates: [
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-          ],
-          supportedLocales: [
-            const Locale('en', 'US'), // English
-            const Locale('ko', 'KO'), // Korean
-          ],
-          title: "GAJUGA.",
-          theme: ThemeData(
-            primaryColor: pale,
-            canvasColor: Colors.transparent,
-          ),
-          themeMode: ThemeMode.light,
+    return FutureBuilder(
+      future: Firebase.initializeApp(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MultiProvider(
+              providers: [
+                ChangeNotifierProvider<FirebaseAuthService>(
+                  create: (_) => FirebaseAuthService(),
+                ),
+                Provider<Information>.value(value: Information()),
+                ChangeNotifierProvider<Sales>(create: (_) => Sales())
+              ],
+              child: MaterialApp(
+                localizationsDelegates: [
+                  GlobalMaterialLocalizations.delegate,
+                  GlobalWidgetsLocalizations.delegate,
+                ],
+                supportedLocales: [
+                  const Locale('en', 'US'), // English
+                  const Locale('ko', 'KO'), // Korean
+                ],
+                title: "GAJUGA.",
+                theme: ThemeData(
+                  primaryColor: pale,
+                  canvasColor: Colors.transparent,
+                ),
+                themeMode: ThemeMode.light,
+                home: Scaffold(
+                  body: MenuPage(),
+                ),
+              ));
+        }
+        if (snapshot.hasError) {
+          return Text("Firebase initalize error !");
+        }
+        return MaterialApp(
           home: Scaffold(
-            body: MenuPage(),
+            body: Center(child: customLoadingBouncingGrid(white)),
           ),
-        ));
+        );
+      },
+    );
   }
 }
